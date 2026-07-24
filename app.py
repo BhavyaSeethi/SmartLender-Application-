f3cda3b02c04e97130a10bce49d6a59889c1448b
BhavyaSeethi <seethibhavya8008@gmail.com>
2026-07-24 11:38:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,4 +26,4 @@ def result():
     return render_template('output.html', name=name, age=age, income=income, prediction=prediction)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+     app.run(host="0.0.0.0", port=8000, debug=True)
